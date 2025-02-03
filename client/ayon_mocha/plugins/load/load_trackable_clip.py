6b230aee1b9e049b00298ed498e7e6ebf41d3ff5
Ondřej Samohel <ondrej.samohel@ynput.io>
2025-02-03 17:46:01 +0100

    :sparkles: add trackable clip loader

---
+++ client/ayon_mocha/plugins/load/load_trackable_clip.py
@@ -0,0 +1,107 @@
+"""Load a clip from a file as trackable clip."""
+from __future__ import annotations
+
+import time
+from typing import TYPE_CHECKING, ClassVar, Optional
+
+from ayon_core.lib.transcoding import IMAGE_EXTENSIONS
+from ayon_core.pipeline import get_representation_path, registered_host
+from ayon_core.pipeline.load import LoadError
+from ayon_mocha.api.lib import update_ui
+from ayon_mocha.api.pipeline import (
+    Container,
+    MochaProHost,
+)
+from ayon_mocha.api.plugin import MochaLoader
+
+if TYPE_CHECKING:
+    from mocha.project import Clip
+
+
+
+class LoadTrackableClip(MochaLoader):
+    """Load a clip from a file."""
+
+    label = "Load Trackable Clip"
+    order = -11
+    icon = "code-fork"
+    color = "orange"
+
+    product_types: ClassVar[set[str]] = {"*"}
+    representations: ClassVar[set[str]] = {"*"}
+    extensions: ClassVar[set[str]] = {
+        ext.lstrip(".") for ext in IMAGE_EXTENSIONS}
+
+    def load(self,
+             context: dict,
+             name: Optional[str] = None,
+             namespace: Optional[str] = None,
+             options: Optional[dict] = None) -> None:
+        """Load a clip from a file as trackable clip."""
+        host: MochaProHost = registered_host()
+        project = host.get_current_project()
+        with project.undo_group():
+
+            current_clip: Clip = project.default_trackable_clip
+            if current_clip is None:
+                msg = "No trackable clip found in the project."
+                raise LoadError(msg)
+            # no way how to change clip name
+            # project.parameter([current_clip, "name"]).set(name)
+
+            file_path = self.filepath_from_context(context)
+            current_clip.relink(file_path)
+
+            for cnt in host.get_containers():
+                if cnt.get("name") == current_clip.name:
+                    cnt["representation"] = str(context["representation"]["id"])
+                    return
+
+            container = Container(
+                name=current_clip.name,
+                namespace=namespace or "",
+                loader=self.__class__.__name__,
+                representation=str(context["representation"]["id"]),
+                objectName=current_clip.name,
+                timestamp=time.time_ns()
+            )
+            host.add_container(container)
+
+    def switch(self, container: dict, context: dict) -> None:
+        """Switch the image sequence on the current camera."""
+        self.update(container, context)
+
+
+    def remove(self, container: dict) -> None:
+        """Remove a container."""
+        host: MochaProHost = registered_host()
+        project = host.get_current_project()
+
+        clips = project.get_clips()
+        clip = clips.get(container["objectName"])
+        if not clip:
+            self.log.warning("Clip %s not found", container["objectName"])
+            return
+        del clip
+        host.remove_container(Container(**container))
+
+
+    def update(self, container: dict, context: dict) -> None:
+        """Update a container."""
+        host: MochaProHost = registered_host()
+
+        version_entity = context["version"]
+        repre_entity = context["representation"]
+
+        file_path = get_representation_path(repre_entity)
+        project = host.get_current_project()
+        clips = project.get_clips()
+        try:
+            clips[container["objectName"]].relink(file_path)
+        except KeyError:
+            self.log.warning("Clip %s not found", container["objectName"])
+        update_ui()
+
+        container["representation"] = repre_entity["id"]
+        container["version"] = str(version_entity["version"])
+        host.add_container(Container(**container))
